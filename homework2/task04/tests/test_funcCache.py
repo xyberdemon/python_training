ee3c6d8693380c39d0a448643f5a2c67336cdb1b
Dmitrii_Bainov <dmitrii_bainov@epam.com>
2020-11-23 18:39:10 +0300

    Homework2 task4 is ready

---
+++ homework2/task04/tests/test_funcCache.py
@@ -0,0 +1,10 @@
+import pytest
+from funcCache.funcCache import cache, func
+
+
+def test_combinations():
+    cache_func = cache(func)
+    some = 100, 200
+    val_1 = cache_func(*some)
+    val_2 = cache_func(*some)
+    assert val_1 is val_2
